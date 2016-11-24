cb2f7712c99925a18ebd6a77148814aa8fcb35e2
Choi <superbag2010@naver.com>
2016-11-24 11:25:16 +0900

    modify tab to space

--- cnn-disease-prediction-tf/disease_cnn.py
+++ cnn-disease-prediction-tf/disease_cnn.py
@@ -3,16 +3,17 @@ import numpy as np
 
 class DiseaseCNN(object):
 	
-	def __init__(self, stride_window_size, num_attributes, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
-		# Plcaeholders for input, output and dropout
-		self.input_x = tf.placeholder(tf.float32, [None, num_attributes], name="input_x")
-		self.inpuy_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-		self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+    def __init__(self, stride_window_size, num_attributes, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
+        # Plcaeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.float32, [None, num_attributes], name="input_x")
+        self.inpuy_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-		# Keeping track of 12 regularization loss (optional)
-		12_loss = tf.constant(0.0)
+        # Keeping track of 12 regularization loss (optional)
+        12_loss = tf.constant(0.0)
 
 
-		# Embedding layer
-		with tf.device('\cpu:0'), tf.name_scope("embedding"):
-			X = random_uniform([None, stride_window_size, num_attributes, 1], -1.0, 1.0), name="X")
+        # Embedding layer
+        with tf.device('\cpu:0'), tf.name_scope("embedding"):
+	    X = random_uniform([None, stride_window_size, num_attributes, 1], -1.0, 1.0), name="X")
+            
