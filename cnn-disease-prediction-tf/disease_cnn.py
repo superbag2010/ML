a98e22e871b4f9b6d4b43f7530c9488474aac347
choi <superbag2010@naver.com>
2016-11-25 17:07:36 +0900

    practice data reshape

--- cnn-disease-prediction-tf/disease_cnn.py
+++ cnn-disease-prediction-tf/disease_cnn.py
@@ -3,13 +3,13 @@ import numpy as np
 
 class DiseaseCNN(object):
 
-    # stride_window_size = the number of previous days to use for one day prediction
+    # window_height = the number of previous days to use for one day prediction
     # num_attr = the number of type of attribute to use for prediction
     # num_classes = the number of label to predict
     # filter_size = list of filter sizes
     # num_filters = the number of filter
 
-    def __init__(self, stride_window_size, num_attr, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
+    def __init__(self, window_height, num_attr, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
         # Plcaeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.float32, [None, num_attr], name="input_x")
         self.inpuy_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
@@ -18,9 +18,9 @@ class DiseaseCNN(object):
         # Keeping track of 12 regularization loss (optional)
         12_loss = tf.constant(0.0)
 
-        # Grouping layer(Grouping input_x by stride_window_size)
-        with tf.device('/cpu:0'), tf.name_scope("Grouping days"):
-           
+        # Grouping layer(Grouping input_x by window_height)
+        with tf.device('/cpu:0'), tf.name_scope("Grouping_days"):
+            
 
 
         for i, filter_size in enumerate(filter_size):
@@ -29,6 +29,5 @@ class DiseaseCNN(object):
                 filter_shape = [filter_size, num_attr, 1, num_filters]
                 W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
                 b = tf.Variable(tf.constant(0.1, shape=[numb_filters]), name="b")
-                conv = tf.nn.conv2d(
-                    input_x
+                #conv = tf.nn.conv2d(
 
--- cnn-disease-prediction-tf/test_data_grouping.py
+++ cnn-disease-prediction-tf/test_data_grouping.py
@@ -4,13 +4,23 @@ import numpy as np
 
 x_attr, y = processing_data.load_data_and_labels("./data/data.csv")
 
-#def __init__(self, stride_window_size, num_attr, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
+#def __init__(self, window_height, num_attr, num_classes, filter_sizes, num_filters, 12_reg_lambda=0.0):
 
-print("x_attr = {}".format(x_attr))
 x_attr = np.array(x_attr)
 num_attr = x_attr.shape[1]
-print("x_attr[3] = {}".format(x_attr[3]))
-#print("x_attr = {}".format(x_attr))
+num_day = x_attr.shape[0]
+window_height = 1
+
+x = list()
+
+for i in range(num_day):
+    index_end = i + window_height
+    x = x + [x_attr[i:index_end]]
+    if (index_end >= num_day):
+        break
+
+print("x[1] = {}".format(x[1]))
+
 
 y = np.array(y)
 num_classes = 15
@@ -23,6 +33,7 @@ dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 # Keeping track of 12 regularization loss (optional)
 #12_loss = tf.constant(0.0)
 
-# Grouping layer(Grouping input_x by stride_window_size)
+# Grouping layer(Grouping input_x by window_height)
 with tf.device('/cpu:0'), tf.name_scope("Grouping_days"):
     print()
+
