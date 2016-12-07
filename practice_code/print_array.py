846e9e1c0e6645c67701867ff34622fe578248eb
Choi <superbag2010@naver.com>
2016-12-07 12:50:20 +0900

    test list index

--- practice_code/print_array.py
+++ practice_code/print_array.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import numpy as np
 
 class student:
@@ -20,3 +22,15 @@ print(array.shape) # print shape
 
 a = [None, 1]
 print(a)
+
+print("===============================")
+
+ls = [None] * 10
+
+for j, l in enumerate(ls):
+    ls[j] = [j, j, j]
+    print ("ls[{}] = {}".format(j, ls[j]))
+
+integer = 0
+ls[integer + 1] = 100
+print(ls[1])
