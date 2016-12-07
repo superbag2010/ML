015f180838c6a4b6f1feb7f76c9eeb4b29711ff5
Choi <superbag2010@naver.com>
2016-12-07 19:35:58 +0900

    modify cost function(MSE->RMSE)

--- cnn-disease-prediction-tf/train.py
+++ cnn-disease-prediction-tf/train.py
@@ -18,9 +18,9 @@ tf.flags.DEFINE_string("out_subdir", "", "state sub-directory")
 tf.flags.DEFINE_string("factor_value", "", "state value of factor to add in file name")
 
 # Model Hyperparameters
-tf.flags.DEFINE_string("num_nodes", "10,8", "The comma-separated number of nodes each of layer (default: '10,8')")
+tf.flags.DEFINE_string("num_nodes", "0", "The comma-separated number of nodes each of layer (default: '0')")
 tf.flags.DEFINE_integer("num_features", 21, "The number of feature attributes")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_string("filter_sizes", "3,4,5,6,7", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.8, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
@@ -121,7 +121,7 @@ with tf.Graph().as_default():
         # Define Training procedure(compute gradient and apply gradient)
         global_step = tf.Variable(0, name="global_step", trainable=False)
         optimizer = tf.train.AdamOptimizer(1e-3)
-        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        grads_and_vars = optimizer.compute_gradients(cnn.RMSE)
         train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
         # TODO : grads_and_vars check
         # train_op is train step
@@ -140,17 +140,17 @@ with tf.Graph().as_default():
 #       out_dir = os.path.abspath(os.path.join(os.path.curdir, "result_disease_cnn", timestamp))
         print("Writing to {}\n".format(out_dir))
 
-        # Summaries for loss and accuracy
-        loss_summary = tf.scalar_summary("loss", cnn.loss)
+        # Summaries for accuracy
+       #loss_summary = tf.scalar_summary("loss", cnn.loss)
         RMSE_summary = tf.scalar_summary("RMSE", cnn.RMSE)
 
         # Train Summaries
-        train_summary_op = tf.merge_summary([loss_summary, RMSE_summary, grad_summaries_merged])
+        train_summary_op = tf.merge_summary([RMSE_summary, grad_summaries_merged])
         train_summary_dir = os.path.join(out_dir, "summaries", "train")
         train_summary_writer = tf.train.SummaryWriter(train_summary_dir, sess.graph)
 
         # Dev summaries
-        dev_summary_op = tf.merge_summary([loss_summary, RMSE_summary])
+        dev_summary_op = tf.merge_summary([RMSE_summary])
         dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
         dev_summary_writer = tf.train.SummaryWriter(dev_summary_dir, sess.graph)
 
@@ -174,12 +174,12 @@ with tf.Graph().as_default():
                 cnn.input_y: y_batch,
                 cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
             }
-            _, step, summaries, loss, RMSE = sess.run(
-             [train_op, global_step, train_summary_op, cnn.loss, cnn.RMSE], feed_dict)
+            _, step, summaries, RMSE = sess.run(
+             [train_op, global_step, train_summary_op, cnn.RMSE], feed_dict)
 
             # print data flow #
             time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, RMSE {:g}".format(time_str, step, loss, RMSE))
+            print("{}: step {}, RMSE {:g}".format(time_str, step, RMSE))
 
             # add train summary
             train_summary_writer.add_summary(summaries, step)
@@ -194,11 +194,11 @@ with tf.Graph().as_default():
               cnn.input_y: y_batch,
               cnn.dropout_keep_prob: 1.0 
             }
-            step, summaries, loss, RMSE = sess.run(
-                [global_step, dev_summary_op, cnn.loss, cnn.RMSE],
+            step, summaries, RMSE = sess.run(
+                [global_step, dev_summary_op, cnn.RMSE],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, RMSE {:g}".format(time_str, step, loss, RMSE))
+            print("{}: step {}, RMSE {:g}".format(time_str, step, RMSE))
             # add dev summary
             if writer:
                 writer.add_summary(summaries, step)
