44d62c1add2e788515f58d70ef7206304f35d9c8
Choi <superbag2010@naver.com>
2016-12-16 11:25:18 +0900

    just flag change

--- cnn-disease-prediction-tf/batch_train_CV.bash
+++ cnn-disease-prediction-tf/batch_train_CV.bash
@@ -8,17 +8,42 @@
 #./train.py --window_height=5 --out_subdir="sigmoid/num_NN_nodes" --num_nodes="140,70,25" --factor_value="140,70,25"
 
 #./train.py --window_height=6 --out_subdir="sigmoid/num_NN_nodes/0/window_height" --num_nodes="0" --factor_value="6" --num_epochs=500 --filter_sizes="1,2,3,4,5,6"
-#./train.py --window_height=5 --out_subdir="sigmoid/num_NN_nodes/0/window_height" --num_nodes="0" --factor_value="5" --num_epochs=500 --filter_sizes="1,2,3,4,5"
-#./train.py --window_height=4 --out_subdir="sigmoid/num_NN_nodes/0/window_height" --num_nodes="0" --factor_value="4" --num_epochs=500 --filter_sizes="1,2,3,4"
 #./train.py --window_height=3 --out_subdir="sigmoid/num_NN_nodes/0/window_height" --num_nodes="0" --factor_value="3" --num_epochs=500 --filter_sizes="1,2,3"
 #./train.py --window_height=2 --out_subdir="sigmoid/num_NN_nodes/0/window_height" --num_nodes="0" --factor_value="2" --num_epochs=500 --filter_sizes="1,2"
 
 
-./train.py --window_height=2 --out_subdir="refined_data/0/" --num_nodes="0" --filter_sizes="1,2"
-./train.py --window_height=2 --out_subdir="refined_data/0/" --num_nodes="0" --filter_sizes="1,2"
-./mean_RMSE.py "../../result_disease_cnn/refined_data/0/"
+
+./train.py --window_height=1 --out_subdir="cold_data/0/height1/" --num_nodes="0" --filter_sizes="1" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/0/height2/" --num_nodes="0" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=3 --out_subdir="cold_data/0/height3/" --num_nodes="0" --filter_sizes="1,2,3" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/250/" --num_nodes="250" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/280,50/" --num_nodes="280,50" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/290,130,30/" --num_nodes="280,50" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
 
 
-./train.py --window_height=2 --out_subdir="refined_data/280/" --num_nodes="280" --filter_sizes="1,2"
-./train.py --window_height=2 --out_subdir="refined_data/280/" --num_nodes="280" --filter_sizes="1,2"
-./mean_RMSE.py "../../result_disease_cnn/refined_data/280/"
+./train.py --window_height=1 --out_subdir="cold_data/0/height1/" --num_nodes="0" --filter_sizes="1" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/0/height2/" --num_nodes="0" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=3 --out_subdir="cold_data/0/height3/" --num_nodes="0" --filter_sizes="1,2,3" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/250/" --num_nodes="250" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/280,50/" --num_nodes="280,50" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+./train.py --window_height=2 --out_subdir="cold_data/290,130,30/" --num_nodes="280,50" --filter_sizes="1,2" --data_file_location="./data/cold_data.csv"
+
+
+
+./mean_RMSE.py "../../result_disease_cnn/cold_data/0/height1"
+./mean_RMSE.py "../../result_disease_cnn/cold_data/0/height2"
+./mean_RMSE.py "../../result_disease_cnn/cold_data/0/height3"
+./mean_RMSE.py "../../result_disease_cnn/cold_data/250/"
+./mean_RMSE.py "../../result_disease_cnn/cold_data/280,50/"
+./mean_RMSE.py "../../result_disease_cnn/cold_data/290,130,30"
+
